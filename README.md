a8faa0d23c58c441ea7bea92b2e4a669e53f2b36
chzhdong <chenzhd2119@gmail.com>
2025-03-29 11:10:37 +0800

    Reinit the repo with QAnything.

--- README.md
+++ README.md
@@ -1,10 +1,155 @@
-# CodeGen
-The CodeGen framework is built on LLMs with RAG.
+# QHistory
 
-# Prerequisit
+QHistory aims to develop a history expert leveraging LLMs based on [QAnything](https://github.com/netease-youdao/QAnything).
+This repo is only used for self learning, without purposes for commercial business.
 
-```bash
-pip install -r requirements.txt
+- [What is QAnything](#what-is-qanything)
+  - [Key features](#key-features)
+  - [Architecture](#architecture)
+- [Latest Updates](#-latest-updates)
+- [Before You Start](#before-you-start)
+- [Getting Started](#getting-started)
+  - [Latest Features Table](#latest-features-table)
+  - [Version 2.0.0 adds detailed optimizations:](#version-200-adds-detailed-optimizations)
+    - [Display of data at each stage:](#display-of-data-at-each-stage)
+    - [Problem fixed](#problem-fixed)
+  - [Comparison of New and Old Parsing Effects](#comparison-of-new-and-old-parsing-effects)
+  - [Installation](#installation)
+    - [Prerequisites](#prerequisites)
+    - [step1: pull qanything repository](#step1-pull-qanything-repository)
+    - [step2: Enter the project root directory and execute the startup command.](#step2-enter-the-project-root-directory-and-execute-the-startup-command)
+    - [step3: start to experience](#step3-start-to-experience)
+    - [API](#api)
+    - [DEBUG](#debug)
+    - [Close service](#close-service)
+  - [Offline Use](#offline-use)
+  - [FAQ](#faq)
+  - [Contributing](#contributing)
+    - [Thanks to all contributors for their efforts](#thanks-to-all-contributors-for-their-efforts)
+    - [Special thanks!](#special-thanks)
+  - [Business contact information：](#business-contact-information)
+- [Roadmap & Feedback](#-roadmap--feedback)
+- [Community & Support](#community--support)
+- [License](#license)
+- [Acknowledgements](#acknowledgments)
+
+## Key features
+
+- Data security, supports installation and use by unplugging the network cable throughout the process. 
+- Supports multiple file types, high parsing success rate, supports cross-language question and answer, freely switches between Chinese and English question and answer, regardless of the language of the file.
+- Supports massive data question and answer, two-stage vector sorting, solves the problem of degradation of large-scale data retrieval, the more data, the better the effect, no limit on the number of uploaded files, fast retrieval speed. 
+- Hardware friendly, defaults to running in a pure CPU environment, and supports multiple platforms such as Windows, Mac, and Linux, with no dependencies other than Docker. 
+- User-friendly, no need for cumbersome configuration, one-click installation and deployment, ready to use, each dependent component (PDF parsing, OCR, embed, rerank, etc.) is completely independent, supports free replacement. 
+- Supports a quick start mode similar to Kimi, fileless chat mode, retrieval mode only, custom Bot mode.
+
+# Before You Start
+**Star us on GitHub, and be instantly notified for new release!**
+![star_us](https://github.com/netease-youdao/QAnything/assets/29041332/fd5e5926-b9b2-4675-9f60-6cdcaca18e14)
+* [🏄 Try QAnything Online](https://qanything.ai)
+* [📚 Try read.youdao.com | 有道速读](https://read.youdao.com)
+* [🛠️ Only use our BCEmbedding(embedding & rerank)](https://github.com/netease-youdao/BCEmbedding)
+* [📖 FAQ](FAQ_zh.md)
+* [👂️Let me hear your voice](https://qanything.canny.io/feature-requests)
+
+# Getting Started
+
+## Installation
+### Prerequisites
+| **System** | **Required item**       | **Minimum Requirement** | **Note**                                                                               |
+|------------|-------------------------|-------------------------|----------------------------------------------------------------------------------------|
+|            | RAM Memory              | >= 20GB                 |                                                                                        |
+| Linux/Mac  | Docker version          | >= 20.10.5              | [Docker install](https://docs.docker.com/engine/install/)                              |
+| Linux/Mac  | docker compose  version | >= 2.23.3               | [docker compose install](https://docs.docker.com/compose/install/)                     |
+| Windows    | Docker Desktop          | >= 4.26.1               | [Docker Desktop for Windows](https://docs.docker.com/desktop/install/windows-install/) |
+
+### step1: pull qanything repository
+```shell
+git clone https://github.com/netease-youdao/QAnything.git
+```
+### step2: Enter the project root directory and execute the startup command.
+* Execute the docker compose start command
+* **The startup process takes about 30 seconds. When the log outputs "qanything backend service is ready!", the startup is complete!** 
+```shell
+cd QAnything
+# Start on Linux
+docker compose -f docker-compose-linux.yaml up
+# Start on Mac
+docker compose -f docker-compose-mac.yaml up
+# Start on Windows
+docker compose -f docker-compose-win.yaml up
 ```
 
-The original datasets come from CodeXGLUE.
+(Note) If the startup fails, you can try changing `docker compose` to `docker-compose`.
+
+### step3: start to experience
+#### Front end
+After successful installation, you can experience the application by entering the following addresses in your web browser.
+
+- Front end address: http://localhost:8777/qanything/
+
+### API
+If you want to visit API, please refer to the following address:
+- API address: http://localhost:8777/qanything/
+- For detailed API documentation, please refer to [QAnything API documentation](docs/API.md)
+
+### DEBUG
+If you want to view the relevant logs, please check the log files in the `QAnything/logs/debug_logs` directory.
+- **debug.log**
+  - User request processing log
+- **sanic_api.log**
+  - Backend service running log
+- **llm_embed_rerank_tritonserver.log**(Single card deployment)
+  - LLM embedding and rerank tritonserver service startup log
+- **llm_tritonserver.log**(Multi-card deployment)
+  - LLM tritonserver service startup log
+- **embed_rerank_tritonserver.log**(Multi-card deployment or use of the OpenAI interface.)
+  - Embedding and rerank tritonserver service startup log
+- rerank_server.log
+  - Rerank service running log
+- ocr_server.log
+  - OCR service running log
+- npm_server.log
+  - Front-end service running log 
+- llm_server_entrypoint.log
+  - LLM intermediate server running log
+- fastchat_logs/*.log
+  - FastChat service running log
+
+### Close service
+```shell
+# Front desk service startup mode like:
+docker compose -f docker-compose-xxx.yaml up  # To close the service, please press Ctrl+C.
+# Backend service startup mode like: 
+docker compose -f docker-compose-xxx.yaml up -d # To close the service, please execute the following command.
+docker compose -f docker-compose-xxx.yaml down
+```
+
+## Offline Use
+If you want to use QAnything offline, you need to deploy the local large model (recommended to use ollama) on the offline machine in advance, and then you can start the service using the following command.
+### Install offline for windows 
+```shell
+# Download the docker image on a networked machine
+docker pull quay.io/coreos/etcd:v3.5.5
+docker pull minio/minio:RELEASE.2023-03-20T20-16-18Z
+docker pull milvusdb/milvus:v2.4.8
+docker pull mysql:8.4
+docker pull xixihahaliu01/qanything-win:v1.5.1  # From [https://github.com/netease-youdao/QAnything/blob/master/docker-compose-windows.yaml#L103] Get the latest version number.
+
+# pack image
+docker save quay.io/coreos/etcd:v3.5.5 minio/minio:RELEASE.2023-03-20T20-16-18Z milvusdb/milvus:v2.4.8 mysql:8.4 xixihahaliu01/qanything-win:v1.5.1 -o qanything_offline.tar
+
+# download QAnything code
+wget https://github.com/netease-youdao/QAnything/archive/refs/heads/master.zip
+
+# Copy the image qanything_offline.tar and the code qany-master.zip to the offline machine 
+cp QAnything-master.zip qanything_offline.tar /path/to/your/offline/machine
+
+# Load image on offline machine 
+docker load -i qanything_offline.tar
+
+# Unzip the code and run it
+unzip QAnything-master.zip
+cd QAnything-master
+docker compose -f docker-compose-win.yaml up
+``` 
+Similarly for other systems, just replace the corresponding image of the system, such as replacing mac with docker-compose-mac.yaml, and linux with docker-compose-linux.yaml
---
+++ qanything_kernel/core/retriever/parent_retriever.py
@@ -0,0 +1,247 @@
+from langchain.retrievers import ParentDocumentRetriever
+from qanything_kernel.core.retriever.vectorstore import VectorStoreMilvusClient
+from qanything_kernel.core.retriever.elasticsearchstore import StoreElasticSearchClient
+from qanything_kernel.connector.database.mysql.mysql_client import KnowledgeBaseManager
+from qanything_kernel.core.retriever.docstrore import MysqlStore
+from qanything_kernel.configs.model_config import DEFAULT_CHILD_CHUNK_SIZE, DEFAULT_PARENT_CHUNK_SIZE, SEPARATORS
+from qanything_kernel.utils.custom_log import debug_logger, insert_logger
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from qanything_kernel.utils.general_utils import num_tokens_embed, get_time_async
+import copy
+from typing import List, Optional, Tuple, Dict
+from langchain_core.documents import Document
+from langchain_core.callbacks import (
+    AsyncCallbackManagerForRetrieverRun,
+)
+from langchain_community.vectorstores.milvus import Milvus
+from langchain_elasticsearch import ElasticsearchStore
+import time
+import traceback
+
+
+class SelfParentRetriever(ParentDocumentRetriever):
+    def set_search_kwargs(self, search_type, **kwargs):
+        self.search_type = search_type
+        self.search_kwargs = kwargs
+        debug_logger.info(f"Set search kwargs: {self.search_kwargs}")
+
+    async def _aget_relevant_documents(
+            self, query: str, *, run_manager: AsyncCallbackManagerForRetrieverRun
+    ) -> List[Document]:
+        """Asynchronously get documents relevant to a query.
+        Args:
+            query: String to find relevant documents for
+            run_manager: The callbacks handler to use
+        Returns:
+            List of relevant documents
+        """
+        debug_logger.info(f"Search: query: {query}, {self.search_type} with {self.search_kwargs}")
+        # self.vectorstore.col.load()
+        scores = []
+        if self.search_type == "mmr":
+            sub_docs = await self.vectorstore.amax_marginal_relevance_search(
+                query, **self.search_kwargs
+            )
+        else:
+            res = await self.vectorstore.asimilarity_search_with_score(
+                query, **self.search_kwargs
+            )
+            scores = [score for _, score in res]
+            sub_docs = [doc for doc, _ in res]
+
+        # We do this to maintain the order of the ids that are returned
+        ids = []
+        for d in sub_docs:
+            if self.id_key in d.metadata and d.metadata[self.id_key] not in ids:
+                ids.append(d.metadata[self.id_key])
+        docs = await self.docstore.amget(ids)
+        if scores:
+            for i, doc in enumerate(docs):
+                if doc is not None:
+                    doc.metadata['score'] = scores[i]
+        res = [d for d in docs if d is not None]
+        sub_docs_lengths = [len(d.page_content) for d in sub_docs]
+        res_lengths = [len(d.page_content) for d in res]
+        debug_logger.info(
+            f"Got child docs: {len(sub_docs)}, {sub_docs_lengths} and Parent docs: {len(res)}, {res_lengths}")
+        return res
+
+    async def aadd_documents(
+            self,
+            documents: List[Document],
+            ids: Optional[List[str]] = None,
+            add_to_docstore: bool = True,
+            parent_chunk_size: Optional[int] = None,
+            es_store: Optional[ElasticsearchStore] = None,
+            single_parent: bool = False,
+    ) -> Tuple[int, Dict]:
+        # insert_logger.info(f"Inserting {len(documents)} complete documents, single_parent: {single_parent}")
+        split_start = time.perf_counter()
+        if self.parent_splitter is not None and not single_parent:
+            # documents = self.parent_splitter.split_documents(documents)
+            split_documents = []
+            need_split_docs = []
+            for doc in documents:
+                if doc.metadata['has_table'] or num_tokens_embed(doc.page_content) <= parent_chunk_size:
+                    if need_split_docs:
+                        split_documents.extend(self.parent_splitter.split_documents(need_split_docs))
+                        need_split_docs = []
+                    split_documents.append(doc)
+                else:
+                    need_split_docs.append(doc)
+            if need_split_docs:
+                split_documents.extend(self.parent_splitter.split_documents(need_split_docs))
+            documents = split_documents
+        insert_logger.info(f"Inserting {len(documents)} parent documents")
+        if ids is None:
+            file_id = documents[0].metadata['file_id']
+            doc_ids = [file_id + '_' + str(i) for i, _ in enumerate(documents)]
+            if not add_to_docstore:
+                raise ValueError(
+                    "If ids are not passed in, `add_to_docstore` MUST be True"
+                )
+        else:
+            if len(documents) != len(ids):
+                raise ValueError(
+                    "Got uneven list of documents and ids. "
+                    "If `ids` is provided, should be same length as `documents`."
+                )
+            doc_ids = ids
+
+        docs = []
+        full_docs = []
+        for i, doc in enumerate(documents):
+            _id = doc_ids[i]
+            sub_docs = self.child_splitter.split_documents([doc])
+            if self.child_metadata_fields is not None:
+                for _doc in sub_docs:
+                    _doc.metadata = {
+                        k: _doc.metadata[k] for k in self.child_metadata_fields
+                    }
+            for _doc in sub_docs:
+                _doc.metadata[self.id_key] = _id
+                _doc.page_content = f"[headers]({_doc.metadata['headers']})\n" + _doc.page_content  # 存入page_content，向量检索时会带上headers
+            docs.extend(sub_docs)
+            doc.page_content = f"[headers]({doc.metadata['headers']})\n" + doc.page_content  # 存入page_content，等检索后rerank时会带上headers信息
+            full_docs.append((_id, doc))
+        insert_logger.info(f"Inserting {len(docs)} child documents, metadata: {docs[0].metadata}, page_content: {docs[0].page_content[:100]}...")
+        time_record = {"split_time": round(time.perf_counter() - split_start, 2)}
+
+        embed_docs = copy.deepcopy(docs)
+        # 补充metadata信息
+        for idx, doc in enumerate(embed_docs):
+            del doc.metadata['title_lst']
+            del doc.metadata['has_table']
+            del doc.metadata['images']
+            del doc.metadata['file_name']
+            del doc.metadata['nos_key']
+            del doc.metadata['faq_dict']
+            del doc.metadata['page_id']
+
+        res = await self.vectorstore.aadd_documents(embed_docs, time_record=time_record)
+        insert_logger.info(f'vectorstore insert number: {len(res)}, {res[0]}')
+        if es_store is not None:
+            try:
+                es_start = time.perf_counter()
+                # docs的doc_id是file_id + '_' + i
+                docs_ids = [doc.metadata['file_id'] + '_' + str(i) for i, doc in enumerate(embed_docs)]
+                es_res = await es_store.aadd_documents(embed_docs, ids=docs_ids)
+                time_record['es_insert_time'] = round(time.perf_counter() - es_start, 2)
+                insert_logger.info(f'es_store insert number: {len(es_res)}, {es_res[0]}')
+            except Exception as e:
+                insert_logger.error(f"Error in aadd_documents on es_store: {traceback.format_exc()}")
+
+        if add_to_docstore:
+            await self.docstore.amset(full_docs)
+        return len(res), time_record
+
+
+class ParentRetriever:
+    def __init__(self, vectorstore_client: VectorStoreMilvusClient, mysql_client: KnowledgeBaseManager, es_client: StoreElasticSearchClient):
+        self.mysql_client = mysql_client
+        self.vectorstore_client = vectorstore_client
+        # This text splitter is used to create the parent documents
+        init_parent_splitter = RecursiveCharacterTextSplitter(
+            separators=SEPARATORS,
+            chunk_size=DEFAULT_PARENT_CHUNK_SIZE,
+            chunk_overlap=0,
+            length_function=num_tokens_embed)
+        # # This text splitter is used to create the child documents
+        # # It should create documents smaller than the parent
+        init_child_splitter = RecursiveCharacterTextSplitter(
+            separators=SEPARATORS,
+            chunk_size=DEFAULT_CHILD_CHUNK_SIZE,
+            chunk_overlap=int(DEFAULT_CHILD_CHUNK_SIZE / 4),
+            length_function=num_tokens_embed)
+        self.retriever = SelfParentRetriever(
+            vectorstore=vectorstore_client.local_vectorstore,
+            docstore=MysqlStore(mysql_client),
+            child_splitter=init_child_splitter,
+            parent_splitter=init_parent_splitter,
+        )
+        self.backup_vectorstore: Optional[Milvus] = None
+        self.es_store = es_client.es_store
+        self.parent_chunk_size = DEFAULT_PARENT_CHUNK_SIZE
+
+    @get_time_async
+    async def insert_documents(self, docs, parent_chunk_size, single_parent=False):
+        insert_logger.info(f"Inserting {len(docs)} documents, parent_chunk_size: {parent_chunk_size}, single_parent: {single_parent}")
+        if parent_chunk_size != self.parent_chunk_size:
+            self.parent_chunk_size = parent_chunk_size
+            parent_splitter = RecursiveCharacterTextSplitter(
+                separators=SEPARATORS,
+                chunk_size=parent_chunk_size,
+                chunk_overlap=0,
+                length_function=num_tokens_embed)
+            child_chunk_size = min(DEFAULT_CHILD_CHUNK_SIZE, int(parent_chunk_size / 2))
+            child_splitter = RecursiveCharacterTextSplitter(
+                separators=SEPARATORS,
+                chunk_size=child_chunk_size,
+                chunk_overlap=int(child_chunk_size / 4),
+                length_function=num_tokens_embed)
+            self.retriever = SelfParentRetriever(
+                vectorstore=self.vectorstore_client.local_vectorstore,
+                docstore=MysqlStore(self.mysql_client),
+                child_splitter=child_splitter,
+                parent_splitter=parent_splitter
+            )
+        # insert_logger.info(f'insert documents: {len(docs)}')
+        ids = None if not single_parent else [doc.metadata['doc_id'] for doc in docs]
+        return await self.retriever.aadd_documents(docs, parent_chunk_size=parent_chunk_size,
+                                                   es_store=self.es_store, ids=ids, single_parent=single_parent)
+
+    async def get_retrieved_documents(self, query: str, partition_keys: List[str], time_record: dict,
+                                      hybrid_search: bool, top_k: int):
+        milvus_start_time = time.perf_counter()
+        expr = f'kb_id in {partition_keys}'
+        # self.retriever.set_search_kwargs("mmr", k=VECTOR_SEARCH_TOP_K, expr=expr)
+        self.retriever.set_search_kwargs("similarity", k=top_k, expr=expr)
+        query_docs = await self.retriever.aget_relevant_documents(query)
+        for doc in query_docs:
+            doc.metadata['retrieval_source'] = 'milvus'
+        milvus_end_time = time.perf_counter()
+        time_record['retriever_search_by_milvus'] = round(milvus_end_time - milvus_start_time, 2)
+
+        if not hybrid_search:
+            return query_docs
+
+        try:
+            # filter = []
+            # for partition_key in partition_keys:
+            filter = [{"terms": {"metadata.kb_id.keyword": partition_keys}}]
+            es_sub_docs = await self.es_store.asimilarity_search(query, k=top_k, filter=filter)
+            es_ids = []
+            milvus_doc_ids = [d.metadata[self.retriever.id_key] for d in query_docs]
+            for d in es_sub_docs:
+                if self.retriever.id_key in d.metadata and d.metadata[self.retriever.id_key] not in es_ids and d.metadata[self.retriever.id_key] not in milvus_doc_ids:
+                    es_ids.append(d.metadata[self.retriever.id_key])
+            es_docs = await self.retriever.docstore.amget(es_ids)
+            es_docs = [d for d in es_docs if d is not None]
+            for doc in es_docs:
+                doc.metadata['retrieval_source'] = 'es'
+            time_record['retriever_search_by_es'] = round(time.perf_counter() - milvus_end_time, 2)
+            debug_logger.info(f"Got {len(query_docs)} documents from vectorstore and {len(es_sub_docs)} documents from es, total {len(query_docs) + len(es_docs)} merged documents.")
+            query_docs.extend(es_docs)
+        except Exception as e:
+            debug_logger.error(f"Error in get_retrieved_documents on es_search: {e}")
+        return query_docs
